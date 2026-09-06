a0b9db859f666f28110d34f04837d4c9e031e289
wangchaozhuanyong <276684684+wangchaozhuanyong@users.noreply.github.com>
2026-09-06 22:13:10 +0800

    chore(core): Keep entity imports consistent across formatters

--- packages/core/src/config/order/default-order-process.ts
+++ packages/core/src/config/order/default-order-process.ts
@@ -4,9 +4,7 @@ import { unique } from '@vendure/common/lib/unique';
 
 import { RequestContext } from '../../api/common/request-context';
 import { TransactionalConnection } from '../../connection/transactional-connection';
-import { OrderLine } from '../../entity/order-line/order-line.entity';
-import { OrderModification } from '../../entity/order-modification/order-modification.entity';
-import { Order } from '../../entity/order/order.entity';
+import { Order, OrderLine, OrderModification } from '../../entity';
 import { Payment } from '../../entity/payment/payment.entity';
 import { ProductVariant } from '../../entity/product-variant/product-variant.entity';
 import { OrderPlacedEvent } from '../../event-bus/events/order-placed-event';
